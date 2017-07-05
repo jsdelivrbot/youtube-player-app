523fb375a9e68cef11ccce02172b704ef24df424
Edward Hong <edward.hong527@gmail.com>
2017-07-05 17:52:14 +1200

    S02L25: List Item Keys

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,7 @@ import VideoListItem from './video_list_item'
 
 const VideoList = ({ videos }) => {
 
-  const videoItems = videos.map(video => <VideoListItem video={video} />)
+  const videoItems = videos.map(video => <VideoListItem key={video.etag} video={video} />)
 
   return (
     <ul className="col-md-4 list-group">
