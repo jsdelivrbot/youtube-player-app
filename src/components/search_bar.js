067e002d32c7f691a102e9afc00a6a959d3dcffc
Edward Hong <edward.hong527@gmail.com>
2017-07-05 16:39:42 +1200

    S01L17: Introduction to State

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' }
+  }
 
   render() {
     return <input onChange={e => console.log(e.target.value)} />
