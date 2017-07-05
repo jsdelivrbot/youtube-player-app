f7747ea70b740ef5e79a45251544a16a6b6d3e4a
Edward Hong <edward.hong527@gmail.com>
2017-07-05 14:55:43 +1200

    S01L09: ReactDOM vs React

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 // Create a new component. This component should produce some HTML
 
@@ -6,4 +7,4 @@ const App = () => <div>Hi!</div>
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 
-React.render(App)
+ReactDOM.render(App)
